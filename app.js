9d466455732c2e9a7283586f728910d382f4f603
Godwin Boi-Ikechukwu <boiikechukwu.godwin@gmail.com>
2023-01-11 14:27:28 -0500

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let test = document.querySelectorAll('.container')
 let checkmark = document.getElementById('checkmark')
-
+let reset = document.getElementById('number')
 
 checkmark.addEventListener('click', function check () {
   for (let i = 0; i < test.length; i++) {
       if ( test[ i ].querySelector( '.dots' ) ) {
-        test[i].classList.toggle('new')
+        test[i].classList.remove('new');
+        reset.innerHTML = 0;
     } 
   }
 })
